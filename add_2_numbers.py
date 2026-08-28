ea4fb80430f363c9d3d7d7adac1124dc9fdaebc4
Subhash Tiwari <99662798+subhashtiw24@users.noreply.github.com>
2026-08-28 20:09:46 +0530

    Add add_2_numbers.py

--- add_2_numbers.py
+++ add_2_numbers.py
@@ -2,7 +2,8 @@
 
 num1 = float(input("Enter ist number: "))
 num2 = float(input("Enter 2nd number: "))
+num3 = float(input("Enter 3rd number: "))
 
-sum_result = num1 + num2
+sum_result = num1 + num2 + num3 
 
 print("The sum is:", sum_result)
